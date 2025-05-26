5979f4fa790854432e12f08c0ecdc53feb181072
rosielockett95 <rosie.lockett4@gmail.com>
2025-05-26 16:45:34 +0100

    removing upload buttons and text when uploading avatar

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const dropArea = document.getElementById("drop-area");
 const inputFile = document.getElementById("input-file");
 const imageView = document.getElementById("img-view");
+const imageIcon = document.getElementById("upload-icon");
+const uploadText = document.getElementById("upload-text");
 
 inputFile.addEventListener("change", uploadImage);
 
 function uploadImage() {
   let imgLink = URL.createObjectURL(inputFile.files[0]);
-  console.log(imgLink);
   imageView.style.backgroundImage = `url(${imgLink})`;
+  imageIcon.classList.add("hidden");
+  uploadText.classList.add("hidden");
+  imageView.classList.remove("hidden");
 }
