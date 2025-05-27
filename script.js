26353a26d7e0cd68912e3a9168eb0e0b32a2eb85
rosielockett95 <rosie.lockett4@gmail.com>
2025-05-27 18:48:22 +0100

    adding change and delete photo button

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ const inputFile = document.getElementById("input-file");
 const imageView = document.getElementById("img-view");
 const imageIcon = document.getElementById("upload-icon");
 const uploadText = document.getElementById("upload-text");
+const changeButtons = document.getElementById("change-btns");
+const removeButton = document.getElementById("remove-btn");
 
 inputFile.addEventListener("change", uploadImage);
+
+function clickListener(e) {
+  e.preventDefault();
+}
 
 function uploadImage() {
   let imgLink = URL.createObjectURL(inputFile.files[0]);
@@ -12,4 +18,16 @@ function uploadImage() {
   imageIcon.classList.add("hidden");
   uploadText.classList.add("hidden");
   imageView.classList.remove("hidden");
+  changeButtons.classList.remove("hidden");
+  inputFile.addEventListener("click", clickListener);
+}
+
+removeButton.addEventListener("click", removeImage);
+
+function removeImage() {
+  imageView.style.backgroundImage = "none";
+  imageIcon.classList.remove("hidden");
+  uploadText.classList.remove("hidden");
+  imageView.classList.add("hidden");
+  changeButtons.classList.add("hidden");
 }
